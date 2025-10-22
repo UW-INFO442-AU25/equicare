f257b4d96d6339875be2d4c70724423f14ae2505
jykuang <jukuang@uw.edu>
2025-10-22 12:27:00 -0700

    minor fixing: typo and placement of comp folder under src

--- src/components/Calendar/calendar.js
+++ src/components/Calendar/calendar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const calandar = () => {
+const calendar = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const calandar = () => {
   )
 }
 
-export default calandar
+export default calendar
