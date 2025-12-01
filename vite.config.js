864bfd387af441aab40561ca45d5bf343ce4e3f3
jykuang <jukuang@uw.edu>
2025-12-01 03:53:41 -0800

    update pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/equicare/',
+  build: {
+    chunkSizeWarningLimit: 1000, // increases the warning limit
+  },
 })
